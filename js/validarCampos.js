c9068df392a6e25752d6151e6df8919a917aac5b
guilhermebarroz <guilhermebarroz@icloud.com>
2024-12-26 15:57:42 -0300

    updates

--- js/validarCampos.js
+++ js/validarCampos.js
@@ -6,6 +6,30 @@ function validarCampos() {
     btnEntrar.disabled = !email || !senha || !validarEmail(email);
 }
 
+function ValidarErroEmail(){
+    const email = document.getElementById("iemail").value;
+    if(!email){
+        document.getElementById("emailObrigatorio").style.display = "block";
+    } else if (email){
+        document.getElementById("emailObrigatorio").style.display = "none";
+    }
+
+    if(validarEmail(email)){
+        document.getElementById("emailInvalido").style.display = "none";
+    } else {
+        document.getElementById("emailInvalido").style.display = "block";
+    }
+}
+
+function ValidarErroSenha(){
+    const senha = document.getElementById("isenha").value;
+    if(!senha){
+        document.getElementById("senhaObrigatoria").style.display = "block"
+    } else{
+        document.getElementById("senhaObrigatoria").style.display = "none"
+    }
+}
+
 function validarEmail(email) {
     return /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/.test(email);
 }
