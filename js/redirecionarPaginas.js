38f9a374d21fd779206752ff0d8660e62c996dca
guilhermebarroz <guilhermebarroz@icloud.com>
2025-01-20 19:56:53 -0300

    updates

--- js/redirecionarPaginas.js
+++ js/redirecionarPaginas.js
@@ -1,9 +1,28 @@
-function login(){
+function login() {
     const btnEntrar = document.getElementById("iEntrar");
-    btnEntrar.addEventListener("click", function (e){
+    
+    btnEntrar.addEventListener("click", function(e) {
         e.preventDefault();
-        window.location.href = "index.html";
+        
+        const email = document.getElementById("iemail").value;
+        const senha = document.getElementById("isenha").value;
+        
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+            .then(response => {
+                window.location.href = "index.html";
+            })
+            .catch(error => {
+                alert(getErrorMessage(error));
+            });
     });
+}
+
+function getErrorMessage(error){
+    if (error.code == 'auth/invalid-login-credentials'){
+        return "Usuário não encontrado"
+    } else {
+        return error.message
+    }
 }
 
 function cadastrar(){
